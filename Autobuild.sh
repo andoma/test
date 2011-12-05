0419939ac36a8c6b3dc3b38822867a2f4ebfd580
Andreas Öman <andreas@lonelycoder.com>
2011-12-05 19:43:09 +0100

    Add Autobuild.sh

---
+++ Autobuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BUILD_API_VERSION=1
+EXTRA_BUILD_NAME=""
+
+while getopts "vht:e:" OPTION
+do
+  case $OPTION in
+      v)
+	  echo $BUILD_API_VERSION
+	  exit 0
+	  ;;
+      h)
+	  echo "This script is intended to be used by the autobuild system only"
+	  exit 0
+	  ;;
+      t)
+	  TARGET="$OPTARG"
+	  ;;
+      e)
+	  EXTRA_BUILD_NAME="$OPTARG"
+	  ;;
+  esac
+done
+
+
+if [[ -z $TARGET ]]; then
+    echo "target (-t) not specified"
+    exit 1
+fi
+
